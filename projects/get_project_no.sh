77b1a1ce129ad07a62f3c8848e2a8d9a707d543d
Awais Malik <malik.awais@gmail.com>
2019-09-08 20:50:24 -0700

    Update get_project_no.sh

--- projects/get_project_no.sh
+++ projects/get_project_no.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-projectIdList="clgx-vsg-qvp-glb-reg-8b94,clgx-vsg-qvp-glb-stg-c6c9,clgx-vsg-qvp-glb-dev-2ee0,clgx-vsg-qvp-glb-sbx-ef92,clgx-jenkins-glb-prd-8ea3,clgx-jenkins-glb-dev-281d,clgx-splunkhf-glb-prd-53b2,clgx-ping-us-sbx-4c7f"
+projectIdList="comma,separated,projectId,list"
 Field_Separator=$IFS
  
 # set comma as internal field separator for the string list
