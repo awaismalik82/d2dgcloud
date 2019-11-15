51aa49a35c79c6610582d7602a0d3917815bca1c
Awais Malik <malik.awais@gmail.com>
2019-11-14 19:15:38 -0800

    Added creationDate and image name to the CSV export

--- projects/get_compute_info_by_project.py
+++ projects/get_compute_info_by_project.py
@@ -1,5 +1,6 @@
 from libcloud.compute.types import Provider
 from libcloud.compute.providers import get_driver
+from datetime import datetime
 
 import sys
 import csv
@@ -15,15 +16,23 @@ driver = cls('', '', project = PROJECT)
 with open('cl_gce_instances.csv', 'a') as csvFile:
 
     for node in driver.list_nodes():     
+        #Get the machine zone ID
         zoneId = node.extra.get("zone")
+        #Get the machine type
         machineType = node.extra.get("machineType")
+        #Check if there is a Local SSD attached
         localSSD = "false"
 
         for disk in node.extra.get("disks"):
             if disk.get("type") == "SCRATCH":
                 localSSD = "true"
+        
+        #Get the creation date for the machine                
+        creationTime = node.extra.get("creationTimestamp")
+        creationDate = creationTime.split("T")[0]
 
-        row = [PROJECT, node.name, zoneId.name, machineType[machineType.rfind("/")+1:], node.state, localSSD]
+        #Write the machine info to the CSV file
+        row = [PROJECT, node.name, zoneId.name, machineType[machineType.rfind("/")+1:], node.state, localSSD, node.image, creationDate]
         writer = csv.writer(csvFile)
         writer.writerow(row)
         print(row)
